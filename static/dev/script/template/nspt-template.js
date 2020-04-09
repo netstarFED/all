948ff8c02c1167f0260320a2f7be6a01390ae13f
liyawei <liyawei@hlims.com>
2020-04-09 13:31:37 +0800

    业务组件按钮添加（edit）修改

--- static/dev/script/template/nspt-template.js
+++ static/dev/script/template/nspt-template.js
@@ -3385,8 +3385,19 @@ var NetstarTemplate = {
 		//获取界面值
 		var templatePageData = data.dialogBeforeHandler(data);
 		var tempalteConfig = templatePageData.config;
-		if(typeof(NetstarTemplate.templates[tempalteConfig.template].fillValues)=='function'){
-			NetstarTemplate.templates[tempalteConfig.template].fillValues(value,tempalteConfig,controllerObj);
+		// lyw 修改 原因登记模板要求通过选中数据修改当前选中块 原来方法会重新给块状表格赋值
+		// if(typeof(NetstarTemplate.templates[tempalteConfig.template].fillValues)=='function'){
+		// 	NetstarTemplate.templates[tempalteConfig.template].fillValues(value,tempalteConfig,controllerObj);
+		// }
+		switch(tempalteConfig.template){
+			case 'limsReg':
+				NetstarTemplate.templates[tempalteConfig.template].editValues(value, tempalteConfig, controllerObj);
+				break;
+			default:
+				if(typeof(NetstarTemplate.templates[tempalteConfig.template].fillValues)=='function'){
+					NetstarTemplate.templates[tempalteConfig.template].fillValues(value,tempalteConfig,controllerObj);
+				}
+				break;
 		}
 	},
 	//根据模板定义的keyfield新增值
